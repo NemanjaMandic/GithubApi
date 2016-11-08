cc8126e7c08f3632d9d8c186224200bb468aa8df
NemanjaMandic <nemanjans84@gmail.com>
2016-11-08 19:05:40 +0100

    retrofit get client

--- app/src/main/java/com/example/nemus/githubapi/rest/APIClient.java
+++ app/src/main/java/com/example/nemus/githubapi/rest/APIClient.java
@@ -1,7 +1,23 @@
 package com.example.nemus.githubapi.rest;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by nemus on 11/8/2016.
  */
 public class APIClient {
+
+    public static final String BASE_URL = "https://api.github.com";
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient(){
+        if(retrofit == null){
+          retrofit = new Retrofit.Builder()
+                  .baseUrl(BASE_URL)
+                  .addConverterFactory(GsonConverterFactory.create())
+                  .build();
+        }
+        return retrofit;
+    }
 }
