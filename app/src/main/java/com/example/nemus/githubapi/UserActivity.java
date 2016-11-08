297648c62a13539d45b5663042f1579ab16892ab
NemanjaMandic <nemanjans84@gmail.com>
2016-11-08 04:06:52 +0100

    UserActivity class

--- app/src/main/java/com/example/nemus/githubapi/UserActivity.java
+++ app/src/main/java/com/example/nemus/githubapi/UserActivity.java
@@ -2,12 +2,34 @@ package com.example.nemus.githubapi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class UserActivity extends AppCompatActivity {
+
+    ImageView avatarImg;
+    TextView userNameTV;
+    TextView followersTV;
+    TextView followingTV;
+    TextView login;
+    TextView email;
+    Button ownedrepos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        avatarImg = (ImageView) findViewById(R.id.avatar);
+        userNameTV = (TextView) findViewById(R.id.username);
+        followersTV = (TextView) findViewById(R.id.followers);
+        followingTV = (TextView) findViewById(R.id.following);
+        login = (TextView) findViewById(R.id.login);
+        email = (TextView) findViewById(R.id.email);
+        ownedrepos = (Button) findViewById(R.id.ownedReposBtn);
     }
+
+
 }
