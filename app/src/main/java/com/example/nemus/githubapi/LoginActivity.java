0076a12fee22cd116ddd7bb973fb9ea42f7778aa
NemanjaMandic <nemanjans84@gmail.com>
2016-11-08 01:31:39 +0100

    added retrofit dependencies to gradle

--- app/src/main/java/com/example/nemus/githubapi/LoginActivity.java
+++ app/src/main/java/com/example/nemus/githubapi/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.nemus.githubapi;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -19,5 +20,13 @@ public class LoginActivity extends AppCompatActivity {
 
         login = (Button) findViewById(R.id.btn_login);
         inputUser = (EditText) findViewById(R.id.input_username);
+    }
+
+    public void getUser(View view){
+        //from LoginActivity go to UserActivity
+        i = new Intent(LoginActivity.this, UserActivity.class);
+        //from LoginActivity pass inputUser param to UserActivity which i access with STRING_I_NEED
+        i.putExtra("STRING_I_NEED", inputUser.getText().toString());
+        startActivity(i);
     }
 }
