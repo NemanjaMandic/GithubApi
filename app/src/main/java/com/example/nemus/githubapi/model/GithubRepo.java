d714f3a816b9985b30d1225620b01c7e968e2699
NemanjaMandic <nemanjans84@gmail.com>
2016-11-11 17:15:15 +0100

    GithubRepo model class

---
+++ app/src/main/java/com/example/nemus/githubapi/model/GithubRepo.java
@@ -0,0 +1,45 @@
+package com.example.nemus.githubapi.model;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by nemus on 11/11/2016.
+ */
+public class GithubRepo {
+    @SerializedName("name")
+    private String name;
+    @SerializedName("description")
+    private String description;
+    @SerializedName("language")
+    private String language;
+
+    public GithubRepo(String language, String description, String name) {
+        this.setLanguage(language);
+        this.setDescription(description);
+        this.setName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+}
